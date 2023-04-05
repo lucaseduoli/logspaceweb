cfa3b38443bf338e15236447959a7743dfe5d826
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-04-04 21:26:59 -0300

    button changed, arrow pending

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -60,17 +60,14 @@ export default function Head() {
                 </p>
               </div>
               <div className="flex w-full max-w-2xl gap-2 mt-10 lg:justify-center lg:mx-auto">
-                <div
-                  className="mt-3 rounded-xl bg-gradient-to-r 
-                    from-[#64abb8] via-[#475dc2] to-[#5b51c4] animate-text p-1 sm:mt-0 sm:ml-3"
-                >
                   <a
                     href="mailto:contact@logspace.ai"
-                    className="items-center bg-pearl block px-8 py-2.5 text-base font-medium text-center text-white transition duration-500 ease-in-out transform rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white hover:bg-white hover:text-majorelly"
+                    className=" flex gap-3 items-center ring-2 bg-pearl px-6 py-2.5 text-base font-medium text-center text-white transition duration-500 ease-in-out rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white hover:ring-white hover:shadow-lg  hover:shadow-[#ffffff50]
+                    from-[#64abb8] via-[#475dc2] to-[#5b51c4]"
                   >
                     Schedule A Meeting
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6" viewBox="0 0 16 16" fill="none"><path fill="currentColor" d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path><path className="" stroke="currentColor" d="M1.75 8H11" stroke-width="1.5" stroke-linecap="round"></path></svg>
                   </a>
-                </div>
               </div>
             </div>
           </div>
